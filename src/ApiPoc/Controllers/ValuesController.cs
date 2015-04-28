1da792eebf1850e30695ad5d5eafbfca995e9830
Andres Moschini <andresmoschini@gmail.com>
2015-04-28 15:13:51 -0300

    Dealing with errors

--- src/ApiPoc/Controllers/ValuesController.cs
+++ src/ApiPoc/Controllers/ValuesController.cs
@@ -17,9 +17,24 @@ namespace ApiPoc.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            if (id == 5)
+            {
+                return new ObjectResult("value");
+            }
+            else
+            {
+                // I prefeer to use HttpResponseException (see http://www.strathweb.com/2015/01/migrating-asp-net-web-api-mvc-6-exploring-web-api-compatibility-shim/#crayon-553fc01e6c93a647073714 )
+                // but it requieres a compatibility layer (see https://www.nuget.org/packages/Microsoft.AspNet.Mvc.WebApiCompatShim/ )
+                // so, probably, it is not a good practice
+                // return new HttpNotFoundResult();
+                // return HttpNotFound();
+                // Or maybe this:
+                var result = new ObjectResult(new { message = "We only have a value with id = 5" });
+                result.StatusCode = 404;
+                return result;
+            }
         }
 
         // POST api/values
