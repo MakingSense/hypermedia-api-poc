3924714deb48bf85cf809477d1e7eca5902b421c
Andres Moschini <andresmoschini@gmail.com>
2015-04-28 13:55:10 -0300

    Add a basic ASP.NET 5 MVC project

---
+++ src/ApiPoc/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.DependencyInjection;
+
+namespace ApiPoc
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
